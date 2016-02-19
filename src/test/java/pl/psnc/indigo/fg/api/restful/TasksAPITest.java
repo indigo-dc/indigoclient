37da0d3736943e57c154db7bb3553fee24a2be76
mkopsnc <michalo@man.poznan.pl>
2016-02-19 13:26:24 +0100

    Removed obsolete code

--- src/test/java/pl/psnc/indigo/fg/api/restful/TasksAPITest.java
+++ src/test/java/pl/psnc/indigo/fg/api/restful/TasksAPITest.java
@@ -177,21 +177,6 @@ public class TasksAPITest {
     
   }
 
-//	@Test
-//	public void testSubmitTask() {
-//		TasksAPI api = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
-//                try {
-//                    Task newTask = new Task();
-//                    newTask.setUser("brunor");
-//                    newTask.setApplication("1");
-//                    newTask.setDescription("hello");
-//                    Task prepTask = api.createTask( newTask );
-//
-//                    Upload upload = api.submitTask( prepTask );
-//                } catch(Exception ex) {
-//                    ex.printStackTrace();
-//                }
-//	}
   @Test
   public void testGetTask() {
     TasksAPI api = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
@@ -214,8 +199,6 @@ public class TasksAPITest {
   public void testGetAllTasks() {
     TasksAPI api = new TasksAPI(BaseAPI.LOCALHOST_ADDRESS);
 
-    // TODO: make sure to set proper task ID below
-    // this one is an arbitrary value from previous calls to
     // TaskAPI
     try {
       Task[] tasks = api.getAllTasks();
