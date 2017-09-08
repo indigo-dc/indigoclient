97eea3945da40b63ff89496c389db98aaca04f46
Michal Owsiak <michal@owsiak.org>
2017-09-08 10:04:16 +0200

    Revert "Reorganisation of sources and adding token and url arguments"
    This reverts commit ec4c6ee68fec96595944ce3aac36bd64be017e41.

--- src/main/java/pl/psnc/indigo/cli/IndigoClient.java
+++ src/main/java/pl/psnc/indigo/cli/IndigoClient.java
@@ -1,16 +1,28 @@
 package pl.psnc.indigo.cli;
 
+import org.apache.commons.cli.*;
+
 public class IndigoClient {
 
-  public static void main(String [] args) {
 
-    ICParser parser = new ICParser(args);
+  public static void main(String [] args) {
+    Options options = new Options();
+    CommandLine cmdline = null;
+
+    options.addOption("help", "help", false, "show help");
+
+    CommandLineParser parser = new PosixParser();
+
     try {
-      parser.parse();
+      cmdline = parser.parse(options, args);
     } catch (Exception ex) {
       System.out.println("\n" + ex.getMessage());
-      System.out.flush();
       System.exit(1);
+    }
+
+    if (cmdline.hasOption("help")) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp( "java -cp ./indigoAPI.jar pl.psnc.indigo.cli.IndigoClient", options );
     }
 
   }
